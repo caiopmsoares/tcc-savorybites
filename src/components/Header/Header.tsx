89b25f9ce282155a84a126af20aa044bf66e0b48
Caio Macedo <caiopierre7@hotmail.com>
2023-06-09 03:05:03 -0300

    Fix netlify 404 error

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,48 +1,48 @@
-import React from 'react'
+import React from "react";
 
 import {
-	HeaderContainer,
-	LogoWrapper,
-	IconsContainer,
-	RoundIcon,
-} from './Header.styles'
-import enIcon from '../../images/english.png'
-import ptIcon from '../../images/portuguese.png'
-import tsIcon from '../../images/translate.png'
-import Logo from '../../images/logo.png'
-import { Link } from 'react-router-dom'
+  HeaderContainer,
+  LogoWrapper,
+  IconsContainer,
+  RoundIcon,
+} from "./Header.styles";
+import enIcon from "../../images/english.png";
+import ptIcon from "../../images/portuguese.png";
+import tsIcon from "../../images/translate.png";
+import Logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
-	return (
-		<HeaderContainer>
-			<div>
-				<LogoWrapper>
-					<Link to="/">
-						<img src={Logo} alt="Logo" />
-					</Link>
-				</LogoWrapper>
-			</div>
-			<div>
-				<IconsContainer>
-					<RoundIcon>
-						<Link to="/">
-							<img src={enIcon} alt="English" />
-						</Link>
-					</RoundIcon>
-					<RoundIcon>
-						<Link to="/homeportuguese">
-							<img src={ptIcon} alt="Portuguese" />
-						</Link>
-					</RoundIcon>
-					<RoundIcon>
-						<Link to="/hometranslated">
-							<img src={tsIcon} alt="Translated" />
-						</Link>
-					</RoundIcon>
-				</IconsContainer>
-			</div>
-		</HeaderContainer>
-	)
-}
+  return (
+    <HeaderContainer>
+      <div>
+        <LogoWrapper>
+          <Link to="/">
+            <img src={Logo} alt="Logo" />
+          </Link>
+        </LogoWrapper>
+      </div>
+      <div>
+        <IconsContainer>
+          <RoundIcon>
+            <Link to="/">
+              <img src={enIcon} alt="English" />
+            </Link>
+          </RoundIcon>
+          <RoundIcon>
+            <Link to="/homeportuguese">
+              <img src={ptIcon} alt="Portuguese" />
+            </Link>
+          </RoundIcon>
+          <RoundIcon>
+            <Link to="/hometranslated">
+              <img src={tsIcon} alt="Translated" />
+            </Link>
+          </RoundIcon>
+        </IconsContainer>
+      </div>
+    </HeaderContainer>
+  );
+};
 
-export default Header
+export default Header;
